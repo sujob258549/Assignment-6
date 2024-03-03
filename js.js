c0d3c378f3ed81bf677ae5867374c510f647fac7
DevSujon <sujonmaia274@gmail.com>
2024-03-03 13:37:53 +0600

    post section show commit

---
+++ js.js
@@ -0,0 +1,5 @@
+const array = ['sujon', 2, 3, 4, 5];
+
+for (const element of array) {
+    console.log(element);
+}
--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const allPost = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const data = await res.json();
     data.posts.forEach(item => {
-        console.log(item)
+        // console.log(item)
 
         const div = document.createElement('div');
-        div.innerHTML =`
+        div.innerHTML = `
         <div class="w-full mb-5 flex flex-col md:flex-row gap-4 bg-slate-200 p-10 rounded-xl">
                     <div class="relative w-[50px]">
                         <img class="p-2 w-28 rounded-md bg-white" src="${item.image}" alt="">
@@ -18,10 +18,10 @@ const allPost = async () => {
                     <div>
                         <div class="flex gap-3 flex-wrap">
                             <h3 class="text-sm inter font-medium"># ${item.category}</h3>
-                            <h3 class="text-sm inter font-medium">Author : Awlad Hossain</h3>
+                            <h3 class="text-sm inter font-medium">Author : ${item.author.name}</h3>
                         </div>
                         <h3 class="text-xl font-bold py-2">
-                            10 Kids Unaware of Their Halloween Costume
+                        ${item.title}
                         </h3>
                         <p class="text-sm inter">It’s one thing to subject yourself to ha Halloween costume mishap
                             because, hey that’s your prerogative</p>
@@ -30,28 +30,82 @@ const allPost = async () => {
                             <div class="flex gap-5">
                                 <div class="flex gap-3">
                                     <img src="./img/logo/massage.png" alt="">
-                                    <p>560</p>
+                                    <p>${item.comment_count}</p>
                                 </div>
                                 <div class="flex gap-3">
                                     <img src="./img/logo/viaews.png" alt="">
-                                    <p>560</p>
+                                    <p>${item.view_count}</p>
                                 </div>
                                 <div class="flex gap-3">
                                     <img src="./img/logo/time.png" alt="">
-                                    <p>560</p>
+                                    <p>${item.posted_time}</p>
                                 </div>
                             </div>
 
-                            <div class="flex justify-end md:mt-0 mt-5">
+                            <div  class="modal-show flex justify-end md:mt-0 mt-5">
                                 <img src="./img/logo/mail.png" alt="">
                             </div>
                         </div>
                     </div>
                 </div>
+                
         `;
+        // console.log(item.description)
         const newDiv = document.getElementById('main-div');
         newDiv.appendChild(div);
 
+
     });
 };
+
+
+const latestPost = async () => {
+    const res = await fetch(' https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+    const data = await res.json();
+    data.forEach(item => {
+        const gridDiv = document.getElementById('grid-div');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="p-5 border-2 border-sky-200 rounded-xl">
+        <img class="bg-slate-200 p-5 rounded-xl w-full" src="${item.cover_image}" alt="">
+        <div class="flex gap-3 items-center mr-5 mt-5">
+            <img src="./img/logo/calender.png" alt="">
+            <p>${item.author.posted_date}</p>
+        </div>
+        <h3 class="text-[18px] font-bold py-2">${item.title}</h3>
+        <p class="py-2">${item.description} </p>
+        <div class="flex gap-3 items-center mr-5">
+            <img class="w-12 rounded-full" src="${item.profile_image}" alt="">
+            <div>
+                <p class="text-sm font-bold">${item.author.name}</p>
+                <p>${item.author.posted_date}</p>
+            </div>
+        </div>
+    </div>
+        
+        `;
+console.log(item)
+        gridDiv.appendChild(div);
+
+    });
+
+    };
+const modalShowCollection = document.getElementsByClassName('modal-show');
+
+for (const modalElement of modalShowCollection) {
+    console.log(modalElement);
+}
+
+
+
+// const allModal = () => {
+//     const modalShow = document.getElementsByClassName('modal-show');
+//     for (const element of modalShow) {
+//         console.log(element);
+//     }
+//     console.log(modalShow)
+// };
+
 allPost();
+latestPost();
+// allModal();
